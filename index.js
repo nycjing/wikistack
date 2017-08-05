b597661d84f31535304d2f3bcfff902f4db17744
Jing Jia <nycjing@gmail.com>
2017-08-04 23:27:20 -0400

    add all middle wares and db setup

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+var express = require ('express');
+var app = express();
+var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+var bodyParser = require('body-parser');
+var path = require('path');
+var models = require('./models');
+
+// templating boilerplate setup
+app.engine('html', nunjucks.render); // how to render html templates
+app.set('view engine', 'html'); // what file extension do our templates have
+nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
+
+// logging middleware
+app.use(morgan('dev'));
+
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
+
+// old start the server
+// var server = app.listen(3000, function(){
+//     console.log('listening on port 3000');
+// });
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+models.User.sync({force: true})
+    .then(function () {
+        return models.Page.sync({force: true})
+    })
+    .then(function () {
+        // make sure to replace the name below with your express app
+        var server =app.listen(3000, function () {
+            console.log('Server is listening on port 3000!');
+        });
+    })
+    .catch(console.error);
---
+++ models/index.js
@@ -0,0 +1,48 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:5432/wikistack',{logging: false});
+
+var Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    urlTitle: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        get() {
+            const title = this.getDataValue('title');
+            return 'http://'  + title;
+        }
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    },
+    date: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    }
+});
+
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    }
+});
+
+module.exports = {
+    db: db,
+    Page: Page,
+    User: User
+};
